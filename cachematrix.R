cc507d73cd5fd0c8e9fc4cf84f4deeebb21c0745
wilsondanar <wilsondanar@gmail.com>
2015-02-22 13:09:14 -0600

    Programming 2 Assignment - Dana Wilson

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The overall description of this assignment is to write a pair
+## of fuctions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix" object that can
+## cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setinverse<- function(inverse) inv <<- inverse
+    getinverse<- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the matrix created
+## with the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse matrix")
+    return(inv)
+  } else {
+    inv <- solve(x$get())
+    x$setinverse(inv)
+    return(inv)
+  }
 }
+
+
